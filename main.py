8af2d42d27bc35e07ca6c96700fd98d31d298b9c
vikctar <vikc.steve@gmail.com>
2026-03-04 01:21:13 +0300

    add tox and other dependencies

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Welcome to Ginja Claims"}
+
+@app.post("/claims")
+async def submit_claim():
+    return {"message": "Claim created"}
+
+@app.get("/claims/{id}")
+async def get_claim(id: int):
+    """Retrieve claim status"""
+    return {"id": id}
